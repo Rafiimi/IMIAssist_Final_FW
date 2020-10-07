d9cce2490d4a9c879893b28ef678d72b7e58888a
ravi.b <ravi.b@ravib-laptop.imidomain.com>
2020-10-07 13:20:01 +0530

    testing

--- Scripts/IMIAssist_Sprint_31_TestCases/IMIAssist_Login_TestCases/test555/Script1602050846332.groovy
+++ Scripts/IMIAssist_Sprint_31_TestCases/IMIAssist_Login_TestCases/test555/Script1602050846332.groovy
@@ -15,3 +15,5 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 
+Mobile.delay(2, FailureHandling.STOP_ON_FAILURE)
+
