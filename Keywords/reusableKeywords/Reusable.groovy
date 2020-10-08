c2d490dee10dbc40fc5f73a9933f14fd56380150
ravi.b <ravi.b@ravib-laptop.imidomain.com>
2020-10-08 13:12:02 +0530

    By Ravi At 1.11

---
+++ Keywords/reusableKeywords/Reusable.groovy
@@ -0,0 +1,51 @@
+package reusableKeywords
+
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+
+import com.kms.katalon.core.annotation.Keyword
+import com.kms.katalon.core.checkpoint.Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.testcase.TestCase
+import com.kms.katalon.core.testdata.TestData
+import com.kms.katalon.core.testobject.TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.util.KeywordUtil
+
+
+import internal.GlobalVariable
+
+public class Reusable {
+
+	@Keyword
+	def Login(){
+		try{
+			String loginURL=GlobalVariable.Url
+			String loginUserName=GlobalVariable.Username
+			String loginPassword=GlobalVariable.Password
+
+			WebUI.openBrowser('')
+
+			WebUI.navigateToUrl(loginURL)
+
+			WebUI.maximizeWindow()
+
+			WebUI.setText(findTestObject('Object Repository/LoginPage_TestCases_Objects/User_Email'), loginUserName)
+
+			WebUI.setText(findTestObject('Object Repository/LoginPage_TestCases_Objects/PasswordTextfiled'), loginPassword)
+
+			WebUI.click(findTestObject('Object Repository/LoginPage_TestCases_Objects/Login_button'))
+
+			WebUI.delay(8)
+		} catch (Exception e) {
+			KeywordUtil.markFailed("Unable to login")
+		}
+	}
+}
+
