b2d7e3af46dd1ab46d43a7ac26b6fb51c002f886
rafishaik.m@imimobile.com <rafishaik.m@imimobile.com>
2020-10-15 20:30:10 +0530

    Added few more objects related cases page

---
+++ Object Repository/Cases_Module_objects/Lastupdatedtime.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Lastupdated time object</description>
+   <name>Lastupdatedtime</name>
+   <tag></tag>
+   <elementGuidId>6dffea28-dd33-4fea-b7eb-7983f126afa9</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@class='font-italic ng-binding']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
