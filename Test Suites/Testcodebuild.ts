2086c2b6aa5a00ee890418ae461dfbd9b10d7da0
rafishaik.m@imimobile.com <rafishaik.m@imimobile.com>
2020-12-21 17:54:14 +0530

    Final merge on ealease 2.2 modification with profile and login screens

--- Test Suites/Testcodebuild.ts
+++ Test Suites/Testcodebuild.ts
@@ -10,10 +10,4 @@
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>41189c75-2876-4193-ad7a-37d9ee12a426</testSuiteGuid>
-   <testCaseLink>
-      <guid>d6f05caf-a7c5-436c-a1f1-5335758ab4d4</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Testwithcb/Tc_01_Test</testCaseId>
-   </testCaseLink>
 </TestSuiteEntity>
